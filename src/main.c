40459c05a87390c55f0620e9a30fa38b0ce03d50
dimohqa99 <mr.roog@yandex.ru>
2018-04-11 14:40:38 +0700

    Add func in main

--- src/main.c
+++ src/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-
+    int i = 0;
+    while ( i == 0) {
+        char q;
+        printf("\t(s) Start or (q) Quit\n");
+        scanf("%c", &q);
+        print_new_board();
+        
     return 0;
 }
