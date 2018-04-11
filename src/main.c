9010fa0dab60367d9cb18d9a89117a3a96376252
dimohqa99 <mr.roog@yandex.ru>
2018-04-11 14:53:08 +0700

    Add cycle in main

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "board.h"
 
 int main() {
-    int i = 0;
-    while ( i == 0) {
-        char q;
+    int k = 0;
+    while ( k == 0) {
+        char status;
         printf("\t(s) Start or (q) Quit\n");
-        scanf("%c", &q);
+        scanf("%c", &status);
         print_new_board();
-        
+            if (status == 's') {
+                k = -1;
+                int exit = 0;
+                while (exit == 0) {
+                    char arr1[4], arr2[4];
+                    printf("Write coordinates or (r) to Restart:\n" );
+                    scanf("%s %s", one_place, two_place);
+                    if (arr1[0] == 'r') {
+                        system("clear");
+                        exit = 1;
+                        main();
+                    }
+                }
+            }
+    }
     return 0;
 }
