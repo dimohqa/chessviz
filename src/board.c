35e2880f9815f29b57f14efeafb8da9e3b529f74
dimohqa99 <mr.roog@yandex.ru>
2018-04-18 14:07:26 +0700

    Change func board

--- src/board.c
+++ src/board.c
@@ -21,5 +21,15 @@ char **board() {
         arr[8][i] = 56 + i;
     }
     
+    for (i = 0;i < 9; i++) {
+        arr[8][i] = 96 + i;
+    }
+    
+    for (i = 1;i < 9; i++) {
+        arr[0][i] = g1[i - 1];
+        arr[7][i] = g1[i - 1] - 32;
+        arr[1][i] = p;
+        arr[6][i] = p - 32;
+    }
     return arr;
 }
